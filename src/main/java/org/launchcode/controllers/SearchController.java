f07a3cfcbdc380385d2043bf85cf061581d962aa
LAnderson-STL <kirsch_17@hotmail.com>
2019-05-26 19:48:26 -0500

    "search handler added and display results is working"

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,10 +1,10 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.launchcode.models.JobData;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +16,7 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
@@ -23,5 +24,36 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
 
+       //list of jobs
+        ArrayList<HashMap<String, String>> jobs;
+
+        //if search = all, find by value passing search term
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        }
+
+        //TODO fix this
+        //if search field is blank, show all jobs
+        else if (searchType.equals("")) {
+           return "search";
+        }
+
+        //search by type and term
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        //per instructions, pass columns and list controller as in other search method
+        model.addAttribute("columns", ListController.columnChoices);
+
+        //pass jobs arrayList of hashmpas
+        model.addAttribute("jobs", jobs);
+
+
+        return "search";
+
+    }
 }
